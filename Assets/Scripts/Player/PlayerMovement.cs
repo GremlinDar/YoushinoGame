dcf94d9ffeb47d7e0a4329b85e7e26033898b2ca
bezsuetiplz <gromkiivopl19@mail.ru>
2025-10-12 20:41:36 +0300

    Настроил проект. Сделал файловую структуру. Добавил объект игрока + скрипт передвижения

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 input;
 
-    }
+    void Awake() => rb = GetComponent<Rigidbody2D>();
 
     void Update()
     {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        input = input.normalized;
+    }
 
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * speed * Time.fixedDeltaTime);
     }
 }
