5761cbc7be89336b703a8d113f475c09207dcc9d
bezsuetiplz <gromkiivopl19@mail.ru>
2025-10-17 21:25:16 +0300

    g

--- Assets/Scripts/Weapons/Aim.cs
+++ Assets/Scripts/Weapons/Aim.cs
@@ -5,7 +5,7 @@ public class AimScreept : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Avake()
     {
-        Cursor.visible = false;  
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
